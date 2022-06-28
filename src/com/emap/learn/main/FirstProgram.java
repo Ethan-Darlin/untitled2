73266d2550afb6b180db652f1153671a9686d81b
Ivan Erokhovets <ivan.erkhovets@mail.ru>
2022-06-28 13:58:55 +0300

    v 1.0

---
+++ src/com/emap/learn/main/FirstProgram.java
@@ -0,0 +1,9 @@
+package com.emap.learn.main;
+
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println(42);
+        System.out.println(5);
+    }
+}
